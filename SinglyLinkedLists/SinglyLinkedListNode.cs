2f263ae635651f9cd0610aa4ba84777faecba373
BlaiseGratton <blaisegratton@gmail.com>
2015-01-20 13:38:53 -0600

    "implemented get/set"

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,20 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return this.next; }
+            set {
+                if (this == value)
+                {
+                    throw new ArgumentException("Can't set Next to this");
+                }
+                next = value;
+            }
         }
 
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return this.value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,8 +45,7 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+            this.value = value;
             // Used by the visualizer:
             allNodes.Add(this);
         }
